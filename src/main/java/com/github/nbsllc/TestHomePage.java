4bb4ce68dfa4cea23e8fadde8e1c86f0ee9756cc
Nick <nick@networkbasedsolutions.com>
2020-04-08 23:23:40 -0700

    Finished initial page objects and a "search" integration test case.
    * Added the ability to retrieve search results from the search results page.
    * TODO: Handle non-US currencies contained in the results.
    * Created a happy path integration test case that covers searching for products.
    * Updated the search keyword test data from "t-shirt" to "dress" for more results.

--- src/main/java/com/github/nbsllc/TestHomePage.java
+++ src/main/java/com/github/nbsllc/TestHomePage.java
@@ -1,7 +1,10 @@
 package com.github.nbsllc;
 
+import com.github.nbsllc.domain.Product;
 import com.github.nbsllc.pages.HomePage;
+import com.github.nbsllc.pages.SearchResultsPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -9,6 +12,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,7 +40,22 @@ public class TestHomePage {
     @Test
     public void testThatSearchingForValidItemReturnsResults() {
         HomePage homePage = HomePage.load(driver);
-        homePage.searchFor("t-shirt");
+        SearchResultsPage searchResultsPage = homePage.searchFor("dress");
+        List<Product> products = searchResultsPage.getSearchResults();
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(products).hasSize(7);
+        softly.assertThat(products).extracting(Product::getPrice)
+            .containsExactlyInAnyOrder(
+                28.98,
+                50.99,
+                30.50,
+                16.40,
+                26.00,
+                16.51,
+                27.00
+            );
+        softly.assertAll();
     }
 
     @Test
--- src/main/java/com/github/nbsllc/pages/SearchResultsPage.java
+++ src/main/java/com/github/nbsllc/pages/SearchResultsPage.java
@@ -1,19 +1,54 @@
 package com.github.nbsllc.pages;
 
 import com.github.nbsllc.domain.Product;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SearchResultsPage extends BasePage {
+    @FindBy(css = "ul.product_list > li")
+    List<WebElement> dblProducts;
+
     public SearchResultsPage(WebDriver driver) {
         super(driver);
         assertThat(driver.getTitle()).isEqualTo("Search - My Store");
     }
 
+    /**
+     * Get a collection of search results (AKA products).
+     *
+     * @return The collection, which could be empty (no results).
+     */
     public List<Product> getSearchResults() {
-        return null;
+        List<Product> products = new ArrayList<>();
+
+        for (WebElement webElement : dblProducts) {
+            String url = webElement.findElement(By.cssSelector("a.product_img_link")).getAttribute("href");
+            String name = webElement.findElement(By.cssSelector("a.product-name")).getText().trim();
+
+            WebElement lblPrice = webElement.findElement(By.cssSelector("span.price"));
+            double price = currencyToDouble(lblPrice.getAttribute("innerHTML").trim());
+
+            products.add(
+                Product.Builder()
+                    .withUrl(url)
+                    .withName(name)
+                    .withPrice(price)
+                    .build()
+            );
+        }
+
+        return products;
+    }
+
+    private double currencyToDouble(String price) {
+        // TODO: Handle non-US currencies.
+        return Double.parseDouble(price.replace("$", ""));
     }
 }
