18ea989b92a40ed20f6dff8ceac5daf0b0eb5f69
Nick <nick@networkbasedsolutions.com>
2020-04-15 12:43:53 -0700

    Added request and response logging.
    * Updated the API test base class to enable logging by default.
    * Simplified the API test case since logging is enabled by default.
    * Added log4j's iostreams dependency to help with logging.

--- src/main/java/com/github/nbsllc/APITestBase.java
+++ src/main/java/com/github/nbsllc/APITestBase.java
@@ -1,9 +1,15 @@
 package com.github.nbsllc;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.io.IoBuilder;
 import org.testng.annotations.BeforeTest;
+
+import java.io.PrintStream;
 
 public class APITestBase {
     protected static Logger logger = LogManager.getLogger();
@@ -11,5 +17,9 @@ public class APITestBase {
     @BeforeTest
     public void configureRestAssured() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        PrintStream loggerStream = IoBuilder.forLogger(logger).setLevel(Level.INFO).buildPrintStream();
+        RestAssured.filters(RequestLoggingFilter.logRequestTo(loggerStream));
+        RestAssured.filters(ResponseLoggingFilter.logResponseTo(loggerStream));
     }
 }
--- src/main/java/com/github/nbsllc/TestUserAPI.java
+++ src/main/java/com/github/nbsllc/TestUserAPI.java
@@ -2,8 +2,6 @@ package com.github.nbsllc;
 
 import com.github.nbsllc.domain.User;
 import io.restassured.http.ContentType;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.apache.logging.log4j.Level;
 import org.testng.annotations.Test;
 
@@ -17,19 +15,15 @@ public class TestUserAPI extends APITestBase {
     public void testThatUserApiReturnsValidData() {
         logger.log(Level.getLevel("STEP"), "Testing that the user API returns a collection of user data.");
 
-        ExtractableResponse<Response> response =
+        List<User> users =
             get("https://jsonplaceholder.typicode.com/users")
                 .then().assertThat()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .extract();
-
-        logger.info(response.asString());
+                .extract().jsonPath().getList("", User.class);
 
         logger.log(Level.getLevel("STEP"), "Verify that the correct number of users are returned.");
         logger.log(Level.getLevel("STEP"), "Verify that the user data is accurate.");
-
-        List<User> users = response.jsonPath().getList("", User.class);
 
         assertThat(users).hasSize(10);
         assertThat(users).extracting(User::getName).contains("Clementine Bauch");
